622b3b2e5f4321e92bda9d0acd21b07a6986e37b
Bruno <nweremizu.bruno@stu.cu.edu.ng>
2024-07-01 14:46:08 +0100

    chore: Update axios dependency to version 1.7.2

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,27 +12,29 @@ app.get("/", (req, res) => {
 
 app.get("/api/hello", async (req, res) => {
   const { visitor_name } = req.query;
-  // get the ip address of the client
+  // Get the IP address of the client
   let ip_address = "";
   let temperature = 0;
   let location = "";
-  await fetch(`https://ipapi.co/json/`)
-    .then((response) => response.json())
-    .then((data) => {
-      ip_address = data?.ip;
-      location = data?.city;
-    });
-  await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=f4823e6d9b3844d0b73112846240107&q=${ip_address}&aqi=no`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      temperature = data?.current.temp_c;
-    });
+  try {
+    const ipResponse = await axios.get("https://ipapi.co/json/");
+    ip_address = ipResponse.data?.ip;
+    location = ipResponse.data?.city;
+
+    const weatherResponse = await axios.get(
+      `https://api.weatherapi.com/v1/current.json?key=f4823e6d9b3844d0b73112846240107&q=${ip_address}&aqi=no`
+    );
+    temperature = weatherResponse.data?.current.temp_c;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Error fetching data" });
+    return;
+  }
+
   res.json({
-    client_ip: `${ip_address}`,
-    location: `${location}`,
-    greeting: `Hello, ${visitor_name}!, the temperature is ${temperature} degrees Celsius in ${location}.`,
+    client_ip: ip_address,
+    location: location,
+    greeting: `Hello, ${visitor_name}! The temperature is ${temperature} degrees Celsius in ${location}.`,
   });
 });
 
